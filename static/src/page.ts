7c9333366727d8473d5723f66b649c131be16932
evandrouzeda <evandrouzeda@gmail.com>
2022-03-30 23:24:45 -0300

    funcionando paginas

--- static/src/page.ts
+++
@@ -1,9 +0,0 @@
-import Component from "./component"
-
-export default interface Page {
-    route: string
-    title?: string
-    children?: Array<Node>
-    auth?: string,
-    params?: { [key: string]: string }
-}
