c8812b139fc7bea03c133d3079c0d364b937f7f2
evandrouzeda <evandrouzeda@gmail.com>
2022-04-01 11:40:19 -0300

    teste com abstracao para criar elementos html

--- public/_js/pages/login.js
+++ public/_js/pages/login.js
@@ -1,21 +1,17 @@
-import ComponentForm from "../components/form/_component.js";
-import RepositoryLocalStorage from "../core/repository/localStorage.js";
-import User from "../features/user/domain/entity/user.js";
-import FormLogin from "../features/user/form/login.js";
+import { $ } from "../ui/zeyo.js";
 export default class Login {
     constructor() {
         this.route = "/";
-        const main = document.createElement("main");
-        main.classList.add("d-grid", "login");
-        this["route"] = "/";
-        const logo = document.createElement("div");
-        logo.classList.add("d-grid", "gap-m", "jc-center", "ac-center", "h-100");
-        main.appendChild(logo);
-        const login = document.createElement("div");
-        login.classList.add("d-grid", "gap-m", "jc-center", "ac-center", "h-100", "login");
-        const repository = new RepositoryLocalStorage();
-        new ComponentForm(login).create(new FormLogin(repository, new User()));
-        main.appendChild(login);
+        const main = new $("main").addClass("d-grid", "login").children(new $("div").addClass("d-grid", "gap-m", "jc-center", "ac-center", "h-100"), new $("div").addClass("d-grid", "gap-m", "jc-center", "ac-center", "h-100", "login")
+            .children(new $("form").object(e => e.element.onsubmit = (e) => {
+            e.preventDefault();
+            console.log(e.target);
+        }).addClass("d-grid", "gap-m").children(new $("h1").object(e => e.element.innerText = "Login"), new $("div").addClass("d-grid", "gap-p").children(new $("label").object(e => e.element.innerText = "Usuario"), new $("input")), new $("div").addClass("d-grid", "gap-p").children(new $("label").object(e => e.element.innerText = "Senha"), new $("input")), new $("button").object(e => e.element.innerText = "Entrar")))).element;
+        /* const login = document.createElement("div")
+        login.classList.add()
+        const repository = new RepositoryLocalStorage()
+        new ComponentForm(login).create(new FormLogin(repository, new User()))
+        main.appendChild(login) */
         this.children = [main];
         console.log(this);
     }
