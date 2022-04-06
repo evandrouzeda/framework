a4c7e91351680bf5d0f9a197c8c78e900647bea4
evandrouzeda <evandrouzeda@gmail.com>
2022-04-06 11:57:41 -0300

    mudando o valor do componente folha

---
+++ public/_js/components/cards/simple.js
@@ -0,0 +1,11 @@
+import { Z } from "../../ui/zeyo.js";
+export default class CardSimple {
+    constructor() {
+        this.main = new Z("div");
+    }
+    create(o) {
+        //Todo: aqui teria que chamar um adapter do tipo presentar para adaptar o objeto para o componente
+        return this.main = new Z("div").children(new Z("h3").text(o.nome)).click(() => o.nome = "clicou");
+    }
+    ;
+}
---
+++ src/components/cards/simple.ts
@@ -0,0 +1,13 @@
+import { Zeyo } from "../../ui/lib.js";
+import { Z } from "../../ui/zeyo.js";
+import Component from "../_component.js";
+
+export default class CardSimple implements Component {
+    main: Z = new Z("div")
+    create(o: any): Zeyo{
+        //Todo: aqui teria que chamar um adapter do tipo presentar para adaptar o objeto para o componente
+        return this.main = new Z("div").children(
+            new Z("h3").text(o.nome)
+        ).click(()=> o.nome = "clicou")
+    };
+}
